24760ed13dfde5c00fc085a71856f404e07a5773
Tufi007 <taufeeqr346@gmail.com>
2023-12-09 20:56:51 +0530

    addedbootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
