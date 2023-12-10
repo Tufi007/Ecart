a090b445fd4a30adbbe88ef0d712e43b253b5ede
Tufi007 <taufeeqr346@gmail.com>
2023-12-10 22:58:28 +0530

    Front end Products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './container/container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
